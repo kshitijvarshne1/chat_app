ce1ec961d711c5d70839896af3ea054000c22e16
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-30 11:36:30 +0530

    create Client3.java

---
+++ src/main/java/com/example/chat_app/Client3.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 30-Apr-21
+ *   Time: 11:36 AM
+ *   File: Client3.java
+ */
+
+package com.example.chat_app;
+
+public class Client3 {
+}
+
