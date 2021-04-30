799835a9e9b65fdd1af8090ad78bc0accd8be55f
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-30 16:08:20 +0530

    add run method

--- src/main/java/com/example/chat_app/Server.java
+++ src/main/java/com/example/chat_app/Server.java
@@ -17,54 +17,77 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Server {
-    private static ArrayList<String> users = new ArrayList<>();
-    private static ArrayList<MessagingThread> clients= new ArrayList<>();
+    private static final ArrayList<String> users = new ArrayList<>();
+    private static final ArrayList<MessagingThread> clients = new ArrayList<>();
 
     public static void main(String[] args) throws IOException, SQLException {
-        ServerSocket server = new ServerSocket(8080,10);
+        ServerSocket server = new ServerSocket(8080, 10);
         System.out.println("Now Serer is running");
         DbOperations.createUsersTable("users");
         DbOperations.createChatTable("chat_backup");
-        while (true){
+        while (true) {
             Socket client = server.accept();
             MessagingThread thread = new MessagingThread(client);
             clients.add(thread);
             thread.start();
         }
     }
-    public static void sendToAll(String user , String message){
+
+    public static void sendToAll(String user, String message) {
         for (MessagingThread c : clients) {
-            if(!c.getUser().equals(user)){
-                c.sendMessage(user,message);
-            }
-            else{
-                c.sendToMe(user,message);
+            if (!c.getUser().equals(user)) {
+                c.sendMessage(user, message);
+            } else {
+                c.sendToMe(user, message);
             }
         }
     }
-    static class MessagingThread extends Thread{
-        String user="";
+
+    static class MessagingThread extends Thread {
+        String user = "";
         BufferedReader input;
         PrintWriter output;
 
         public MessagingThread(Socket client) throws IOException, SQLException {
-            input= new BufferedReader(new InputStreamReader(client.getInputStream()));
-            output=new PrintWriter(client.getOutputStream(),true);
+            input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            output = new PrintWriter(client.getOutputStream(), true);
 
-            user=input.readLine();
+            user = input.readLine();
             users.add(user);
             DbOperations.addUserInDB(user);
         }
 
         public void sendMessage(String user, String message) {
-            output.println(user+" : "+message);
+            output.println(user + " : " + message);
         }
+
         public void sendToMe(String user, String message) {
-            output.println("You: "+message);
+            output.println("You: " + message);
         }
 
         public String getUser() {
             return user;
+        }
+
+        @Override
+        public void run() {
+            String line;
+            try {
+                while (true) {
+                    line = input.readLine();
+                    if(line.equals("end")){
+                        clients.remove(this);
+                        users.remove(user);
+                    }
+                    else{
+                        sendToAll(user,line);
+                        sendToMe(user,line);
+                    }
+
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
