7f7434e32ee4d40660c1f7674b46123b11502b7f
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-30 13:39:13 +0530

    add method createUsersTable and createChatTable

--- src/main/java/com/example/chat_app/DbOperations.java
+++ src/main/java/com/example/chat_app/DbOperations.java
@@ -10,6 +10,7 @@ package com.example.chat_app;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DbOperations {
     private static volatile Connection connection;
@@ -17,11 +18,22 @@ public class DbOperations {
     public static Connection getConnection() throws SQLException {
 
         if (connection == null) {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/chat_application", "root", "chaman@123");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/chat_application", "root", "");
         }
-
         return connection;
+    }
 
+    public static void createUsersTable(String users) throws SQLException {
+        getConnection();
+        Statement statement = connection.createStatement();
+        String sql = "CREATE TABLE " + users + "(id int primary key auto_increment, name VARCHAR(30), joining_time date)";
+        statement.execute(sql);
+    }
+
+    public static void createChatTable(String chat_backup) throws SQLException {
+        getConnection();
+        Statement statement =connection.createStatement();
+        String sql= "CREATE TABLE "+chat_backup+" (msg_id VARCHAR(40) primary key, name VARCHAR(30), msg VARCHAR(200))";
     }
 }
 
